039d16c23d488b2982df1dd2fadcf1d58e869420
Syed Musab Mohiuddin <98490330+itsmekirx909@users.noreply.github.com>
2022-03-31 11:49:11 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,40 @@
+// chap13-15
+
+// Q1
+// var arr = []
+
+// Q2
+// var arr = new Array()
+
+// Q3
+// var stringArr = ['' , '']
+
+// Q4
+// var numArr = [0 , 1]
+
+// Q5
+// var booleanArr = [true , false]
+
+// Q6
+// var mixedArr = ['' , 1 ,false]
+
+// Q7
+// var arr = [ "SSC", "HSC", "BCS", "BS", "BCOM", "MS", "M. Phil.", "PhD"]
+// document.write('<h1>Qualifications<h1/> <br/>')
+// for(var i = 0 ; i < arr.length ; i++){
+//      document.write( (i+1)+')' + arr[i] + '<br/>')
+
+// }
+
+// Q8
+// var students = ['Michael' , 'John' , 'Tony']
+// var totalMark = [320 , 230 , 480]
+// var per1 = (totalMark[0]/500)*100
+// var per2 = (totalMark[1]/500)*100
+// var per3 = (totalMark[2]/500)*100
+// var perr = [per1 , per2 , per3]
+
+// for(var i = 0 ; i < students.length ; i++){
+//     document.write('Score of ' + students[i] + ' is ' + totalMark[i] + '.' + ' Percentage is: ' + perr[i])
+// }
+
